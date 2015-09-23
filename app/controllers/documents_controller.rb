ae68786942c7e474e406bead7b58c24e5f233a5f
Herine Chin <herine.chin@gmail.com>
2015-09-23 10:07:36 -0700

    change resources from dashboard to documents

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,8 +1,12 @@
-class DashboardController < ApplicationController
+class DocumentsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
     @documents = current_user.documents
+  end
+
+  def new
+    @document = Document.new
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :dashboard, :only => [:index]
+  resources :users do
+    collection do
+      resources :documents, only: [:new]
+    end
+    member do
+      resources :documents, except: [:new]
+    end
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
