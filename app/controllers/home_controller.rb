7b34849f6ee9887ed480713dc678e2a9c1b808a1
Herine Chin <herine.chin@gmail.com>
2015-09-23 21:56:57 -0700

    fix redirect bug

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     if user_signed_in?
-      redirect_to documents_path(current_user.id)
+      redirect_to user_documents_path(current_user.id)
     end
   end
 
