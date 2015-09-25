5faf9925c4f720757e25145b8c9fdf64f7be7037
Herine Chin <herine.chin@gmail.com>
2015-09-25 01:34:17 -0700

    add pdf document tests

--- spec/controllers/document_controller_spec.rb
+++ spec/controllers/document_controller_spec.rb
@@ -6,9 +6,9 @@ describe DocumentsController, :type => :controller do
 
   let! (:user) { User.create email: 'test@test.com', password: 'testpassword' }
 
-   before :each do
+  before :each do
     sign_in user
-   end
+  end
 
   describe "#index" do
     subject { get :index, user_id: user.id }
@@ -16,18 +16,18 @@ describe DocumentsController, :type => :controller do
     it { is_expected.to render_template :index }
 
     it { is_expected.to have_content 'Documents Dashboard' }
-    it { is_expected.to have_content 'Loan Amount'}
-    it { is_expected.to have_content 'Down Payment'}
-    it { is_expected.to have_content 'Interest Rate'}
+    it { is_expected.to have_content 'Loan Amount' }
+    it { is_expected.to have_content 'Down Payment' }
+    it { is_expected.to have_content 'Interest Rate' }
     it { is_expected.to have_link 'New Document', new_document_path }
-    it { is_expected.to_not have_content '$'}
-    it { is_expected.to_not have_content '%'}
+    it { is_expected.to_not have_content '$' }
+    it { is_expected.to_not have_content '%' }
 
     context "when there are documents" do
       let! (:documents) { 3.times.collect { |i| Document.create! "loan_amount" => i, "down_payment" => i, "interest_rate" => i, "user_id" => user.id } }
 
-    it { is_expected.to render_template :_document_table }
-    it { is_expected.to render_template :_document_table_row }
+      it { is_expected.to render_template :_document_table }
+      it { is_expected.to render_template :_document_table_row }
       it { is_expected.to have_content '$' }
       it { is_expected.to have_content '%' }
       it { is_expected.to have_link 'View PDF' }
---
+++ spec/models/pdf_document_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe PdfDocument do
+  context 'when given a document object' do
+
+    let!(:user) { User.create email: 'test@test.com', password: 'testpassword' }
+    let!(:document) { Document.create user_id: user.id, loan_amount: 12, interest_rate: 8.88, down_payment: 34 }
+    let(:view_context) { ActionController::Base.new.view_context }
+    let(:pdf) { PdfDocument.new(document, view_context) }
+    let(:pdf_content) {  PDF::Inspector::Text.analyze( pdf.render ) }
+
+    it 'contains the loan amount' do
+      binding.pry
+      expect(pdf_content.strings).to include('$12')
+    end
+
+    it 'contains the down_payment' do
+      expect(pdf_content.strings).to include('$34')
+    end
+
+    it 'contains the interest_rate' do
+      expect(pdf_content.strings).to include('8.88%')
+    end
+
+  end
+end
